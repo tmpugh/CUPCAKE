a696b2ddd66c0a025dcaf50c7f098bcb603f6185
tmpugh <35498721+tmpugh@users.noreply.github.com>
2018-05-08 21:58:12 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -12,23 +12,9 @@ include('includes/header.php');
 require('db_connect.php');
   ?>
 
-<!--    <img src="images/Celebrations.jpg" width="1000">-->
+
 <div id="pictureBox"></div>
-<!--<div id="home">-->
-<!--            <div id="deal">-->
-<!--                <h1>Deal of the Day</h1>-->
-<!--                <h3>The Sweet Split - 25% off!</h3>-->
-<!--                <img src="Images/sweet-split.jpg" alt="Sweet Split" width="200" height="150"/>-->
-<!--                <p>Vanilla, chocolate and strawberry ice cream, chocolate syrup, marshmallow and pineapple toppings, real whipped cream, nuts and a cherry all atop a banana.</p>-->
-<!--            </div>-->
-<!---->
-<!--            <div id="social">-->
-<!--               <h3>Follow us on social media!</h3>-->
-<!--               <img src="Images/facebook.png" alt="Facebook" width="65" height="65"/>-->
-<!--                <img id="twitter" src="Images/twitter.png" alt="Twitter" width="70" height="70"/>-->
-<!--                <img src="Images/instagram.png" alt="Instagram" width="70" height="70"/>-->
-<!--            </div>-->
-<!--        </div>-->
+
 
 
 <?php
